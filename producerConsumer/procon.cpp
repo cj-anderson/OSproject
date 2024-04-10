4d5bcafce581e6b715eeb954f2781dd0aa4a54bb
Prestin Bell <Prestin97@gmail.com>
2024-04-10 12:05:53 -0400

    updated file

--- producerConsumer/procon.cpp
+++ producerConsumer/procon.cpp
@@ -1,114 +1,79 @@
 #include <iostream>
-#include <pthread.h>
-#include <queue>
-#include <unistd.h> // for sleep function
+#include <fstream>
+#include <vector>
+#include <thread>
+#include <mutex>
 #include <chrono>
+#include <random>
+#include <sstream>
 
-using namespace std;
 
-#define MAX_BUFFER_SIZE 10
+std::vector<int> buffer;
+int bufferSize = 5;
+std::mutex bufferMutex;
 
-/*
-struct producerConsumer
-{
-    int id;
-    int priority;
-    int time;
+class ProducerThread {
+public:
+    void operator()() {
+        while (true) {
+            {
+                std::lock_guard<std::mutex> lock(bufferMutex);
+                if (buffer.size() < bufferSize) {
+                    int item = rand() % 100 + 1;
+                    buffer.push_back(item);
+                    std::cout << "Producer produced: " << item << std::endl;
+                }
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 5000 + 1000));
+        }
+    }
 };
-*/
 
-// Shared data structures
-queue<int> buffer;
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t buffer_empty = PTHREAD_COND_INITIALIZER;
-pthread_cond_t buffer_full = PTHREAD_COND_INITIALIZER;
+class ConsumerThread {
+public:
+    void operator()() {
+        while (true) {
+            {
+                std::lock_guard<std::mutex> lock(bufferMutex);
+                if (!buffer.empty()) {
+                    int item = buffer.front();
+                    buffer.erase(buffer.begin());
+                    std::cout << "Consumer consumed: " << item << std::endl;
+                }
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 5000 + 1000));
+        }
+    }
+};
 
-// Function prototypes
-void* producer(void* arg);
-void* consumer(void* arg);
-
-int main(int argc, char* argv[]) {
-    if (argc != 4) {
-        cout << "Usage: " << argv[0] << " <num_producers> <num_consumers> <sleep_time>" << endl;
+int main() {
+    std::string filePath = "producerConsumer/datafile.txt";
+    std::ifstream file(filePath);
+    if (!file.is_open()) {
+        std::cerr << "Error opening file." << std::endl;
         return 1;
     }
 
-    int num_producers = atoi(argv[1]);
-    int num_consumers = atoi(argv[2]);
-    int sleep_time = atoi(argv[3]);
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        int numProducers, numConsumers;
+        if (!(iss >> numProducers >> numConsumers)) {
+            std::cerr << "Error reading file." << std::endl;
+            return 1;
+        }
 
-    pthread_t producer_threads[num_producers];
-    pthread_t consumer_threads[num_consumers];
+        for (int i = 0; i < numProducers; ++i) {
+            std::thread producerThread(ProducerThread{});
+            producerThread.detach();
+        }
 
-    // Create producer threads
-    for (int i = 0; i < num_producers; ++i) {
-        pthread_create(&producer_threads[i], NULL, producer, NULL);
+        for (int i = 0; i < numConsumers; ++i) {
+            std::thread consumerThread(ConsumerThread{});
+            consumerThread.detach();
+        }
     }
 
-    // Create consumer threads
-    for (int i = 0; i < num_consumers; ++i) {
-        pthread_create(&consumer_threads[i], NULL, consumer, NULL);
-    }
-
-    // Join producer threads
-    for (int i = 0; i < num_producers; ++i) {
-        pthread_join(producer_threads[i], NULL);
-    }
-
-    // Join consumer threads
-    for (int i = 0; i < num_consumers; ++i) {
-        pthread_join(consumer_threads[i], NULL);
-    }
-
+    file.close();
     return 0;
-}
-
-void* producer(void* arg) {
-    while (true) {
-        pthread_mutex_lock(&mutex);
-
-        // Produce item
-        int item = rand() % 100;
-
-        // Wait if buffer is full
-        while (buffer.size() == MAX_BUFFER_SIZE) {
-            pthread_cond_wait(&buffer_full, &mutex);
-        }
-
-        // Add item to buffer
-        buffer.push(item);
-        cout << "Produced: " << item << endl;
-
-        // Signal consumers
-        pthread_cond_signal(&buffer_empty);
-
-        pthread_mutex_unlock(&mutex);
-
-        sleep(1); // sleep for some time
-    }
-    return NULL;
-}
-
-void* consumer(void* arg) {
-    while (true) {
-        pthread_mutex_lock(&mutex);
-
-        // Wait if buffer is empty
-        while (buffer.empty()) {
-            pthread_cond_wait(&buffer_empty, &mutex);
-        }
-
-        // Consume item
-        int item = buffer.front();
-        buffer.pop();
-        cout << "Consumed: " << item << endl;
-
-        // Signal producers
-        pthread_cond_signal(&buffer_full);
-
-        pthread_mutex_unlock(&mutex);
-
-        sleep(1); // sleep for some time
-    }
-    return NULL;
 }
